2cd534c3ef08ebcf6ea54cbaf3de05975af8b311
Daniel <popov-ac420@yandex.com>
2023-11-26 16:30:07 +0500

    Первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,30 @@
 "use strict"
+
+/* --- Решение квадратных уравнений --- */
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let result = [];
+	
+	let discriminant = b ** 2 - 4 * a * c;
+	
+	if (discriminant < 0) {
+		result = [];
+	} else if (discriminant === 0) {
+		result = [-b / (2 * a)];
+    } else {
+		result = [(-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a)];
+	}
+
+	return result;
 }
 
+
+
+/* --- Калькулятор расчёта выплат по ипотеке --- */
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	percent /= 100 * 12;
+	let mainPayment = amount - contribution;
+	let monthPayment = mainPayment * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
+	let totalPayment = Number((monthPayment * countMonths).toFixed(2));
+	
+	return totalPayment;
 }
