b2d6c82e7a520408e1ad8c0c0d5724caaf8a2fca
Daniel <popov-ac420@yandex.com>
2023-12-07 19:07:40 +0500

    Четвёртое задание -- Правки #1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -13,29 +13,23 @@ Student.prototype.setSubject = function (subjectName) {
 
 /* --- #3 --- */
 Student.prototype.addMarks = function (...marksToAdd) {
-	if (!this.excluded) {
-		this.marks = [...marksToAdd];
+	if (this.marks) {
+		this.marks.push(...marksToAdd);
 	}
 }
 
 /* --- #4 --- */
 Student.prototype.getAverage = function () {
-//	if (!this.hasOwnProperty(marks)) {
-//		return average = 0;
-//	} else 
-	
-//	if (marks in this === false) {
-//		return average = 0;
-//	} else 
-	if (this.marks.length !== 0) {
-		let sum = this.marks.reduce(function(sum, number) {
-			return sum + number;
+	if ("marks" in this === false) {
+		return 0;
+	} else if (this.marks.length !== 0) {
+		let sum = this.marks.reduce(function(sum, mark) {
+			return sum + mark;
 		}, 0);
-		
-		let average = sum / this.marks.length;
-		return average;	
-	} else  {
-		return average = 0;
+
+		return sum / this.marks.length;	
+	} else {
+		return 0;
 	}
 }
 
@@ -45,22 +39,3 @@ Student.prototype.exclude = function (reason) {
 	delete this.marks;
 	this.excluded = reason;
 }
-
-
-let student = new Student("Василиса", "женский", 19);
-student.exclude('прогулы');
-
-student.addMarks(5, 5, 5);
-console.log(student);
-
-student.addMarks(3, 2, 5);
-console.log(student);
-
-//student.getAverage();
-console.log(student);
-
-//console.log(student.hasOwnProperty(name));
-//console.log(student.hasOwnProperty(age));
-//
-//console.log(name in student);
-//console.log(age in student);
