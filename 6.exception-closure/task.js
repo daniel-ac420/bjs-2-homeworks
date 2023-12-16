5e630f53e8bf7189d508da1e5237a89ae3316646
Daniel <popov-ac420@yandex.com>
2023-12-16 20:29:24 +0500

    Шестое задание

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,74 @@
-﻿
+﻿/* --- #1 --- */
+
+function parseCount(value) {
+	if (Number.isNaN(Number.parseFloat(value))) {
+		throw new Error("Невалидное значение");
+	} else {
+		return Number.parseFloat(value);	
+	}
+}
+
+function validateCount(value) {
+	try {
+		parseCount(value);
+	} catch(error) {
+		console.error(error);
+		return error;
+	} finally {
+		return parseCount(value);
+	}
+}
+
+
+
+/* --- #2 --- */
+
+class Triangle {
+	constructor(sideA, sideB, sideC) {
+		this.sideA = sideA;
+		this.sideB = sideB;
+		this.sideC = sideC;
+		
+		if (sideA + sideB <= sideC || 
+		   sideA + sideC <= sideB ||
+		   sideB + sideC <= sideA) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+		
+		this.perimeter = sideA + sideB + sideC;
+		this.area = Number(Math.sqrt(this.perimeter * (this.perimeter / 2 - sideA) * (this.perimeter / 2 - sideB) * (this.perimeter / 2 - sideC) / 2).toFixed(3));
+	}
+	
+//	set perimeter(perimeterValue) {
+//		this._perimeter = perimeterValue;
+//	}
+	
+	set perimeter(perimeterValue) {
+		if (perimeterValue !== this.perimeter) {
+			this._perimeter = this.perimeter;	
+		} else {
+			this._perimeter = perimeterValue;
+		}	
+	}
+	
+	get perimeter() {
+		return this._perimeter;
+	}
+	
+	set area(areaValue) {
+		this._area = areaValue;
+	}
+
+	get area() {
+		return this._area;
+	}
+}
+
+
+function getTriangle(sideA, sideB, sideC) {
+	try {
+		return new Triangle(sideA, sideB, sideC);
+	} catch(error) {
+		return "Ошибка! Треугольник не существует";
+	}
+}
